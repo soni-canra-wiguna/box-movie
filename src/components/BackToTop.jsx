9557ee76a7cb8cb26d46a9e6e5b20c5349f7436e
soni-canra-wiguna <sonicandra0511@gmail.com>
2023-09-11 17:44:04 +0700

    feat: "Add shadow to btn back to top for improved"

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -9,7 +9,7 @@ const BackToTop = () => {
   const [visibleButton, setVisibleButton] = useState(false)
 
   const handleScroll = () => {
-    if (window.scrollY > 900) {
+    if (window.scrollY > 850) {
       setVisibleButton(!visibleButton)
     } else {
       setVisibleButton(false)
@@ -33,17 +33,17 @@ const BackToTop = () => {
 
   return (
     <motion.div
-      className={`  ${
+      className={`${
         visibleButton
           ? "block opacity-80 transition duration-500 ease-in md:opacity-100"
           : "hidden opacity-0 transition duration-500 ease-in"
-      } group fixed bottom-[1.2rem] left-1/2 z-[59] -translate-x-1/2 transform transition-all duration-500 ease-in-out`}
+      } group fixed bottom-[1.2rem] left-1/2 z-[50] -translate-x-1/2 transform transition-all duration-500 ease-in-out`}
       style={{ position: "fixed", translateX: "-50%" }}
     >
       <motion.button
         whileTap={{ scale: 0.9 }}
         onClick={handleToTop}
-        className="flex items-center space-x-1.5 rounded-full border border-black/70 bg-gradient-to-l from-blue-600 to-teal-600 px-2 py-2 text-xs opacity-80 md:text-sm md:opacity-100"
+        className="shadow__to-top flex items-center space-x-1.5 rounded-full bg-gradient-to-l from-blue-600 to-teal-600 px-2 py-2 text-xs opacity-90 md:text-sm"
       >
         <FaArrowUp className="fill-black" />{" "}
         <span className="font-medium text-black">back to top</span>
